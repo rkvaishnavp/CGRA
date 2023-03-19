29a74cab59e9facfdc5c7795b021b63b31c29917
manav-agarwal1 <manavagarwal001@gmail.com>
2023-03-19 15:27:44 +0530

    Added DSP48e1 instantiation
    Modified data bits for 4 inputs
    TODO: modify parts of instruction where we give control bits to DSP48E1.

--- updated_tile/tile.v
+++ updated_tile/tile.v
@@ -9,9 +9,11 @@ Types of Instruction: [2:0]
 
 5 bit for source 1 [6:3]
 5 bit for source 2 [10:7]
-5 bit for Destination Register [14:11]
+5 bit for source 3 [14:11]
+5 bit for source 4 [18:15]
+5 bit for Destination Register [22:19]
 
-Direction Bit [17:15]
+Direction Bit [25:23]
     N   -   000
     NE  -   001
     E   -   010
@@ -22,17 +24,17 @@ Direction Bit [17:15]
     NW  -   111
     
 Interact with Data Memory:
-Address                     [27:18]
-Tile Reg                    [34:30]
+Address                     [35:26]
+Tile Reg                    [40:36]
 
 TODO:
 Interact with other tile:
 Can only interact with 8 nhbrs
-5-bit Tile data reg address      [38:35]
+5-bit Tile data reg address      [44:41]
 
 DSP48E:
-Inputs: [43:39]
-    input bits = 3 bit for sel, 2 for mode (may vary)
+Inputs: ????? TODO Data bits for DSP48e will change'
+    // Will do as per datasheet
     A: The first input to the ALU. It can be a signed or unsigned value of up to 25 bits.
     B: The second input to the ALU. It can be a signed or unsigned value of up to 18 bits.
     C: The carry-in input to the ALU. It can be a 1-bit value.
@@ -96,7 +98,9 @@ output reg [63:0] instruction_out
 integer i = 0;
 parameter n = 8;
 parameter m = 8;
-// 32, 8 bit registers
+// n, 32 bit registers
+// TODO: change to 48 as largest data in ALU is 48 bit
+// or we can keep them 0
 reg [31:0] registers [n-1:0];
 
 // 8, 64 bit instruction memory
@@ -110,21 +114,110 @@ reg data_mem_rd_valid;
 // _ _ _ _ _ _ Z C
 reg [8:0] flag;
 reg [49:0] ALUOUT;
-wire [31:0] operand1 = registers[instruction[6:3]];
-wire [31:0] operand2 = registers[instruction[10:7]];
+wire [31:0] operand1 = registers[instruction[6:3]]; // A
+wire [31:0] operand2 = registers[instruction[10:7]]; // B
+wire [31:0] operand3 = registers[instruction[14:11]]; // C
+wire [31:0] operand4 = registers[instruction[18:15]]; // D
 wire alu_en = (instruction[2:0] == 3'b0) ? 1 : 0;
 
 
 // In program mode, we fill the memory
 // In execution we execute so, we should set ip to 0
 // to start from first instruction
-always @(posedge program_mode or negedge program_mode)begin
+always @(program_mode)begin
     ip = 4'b0;
     instruction_wrt = 1'b0;
 end
 
 // TODO: Call to DSP48E
-
+DSP48E1 #(
+  // Feature Control Attributes: Data Path Selection
+  .A_INPUT("DIRECT"),               // Selects A input source, "DIRECT" (A port) or "CASCADE" (ACIN port)
+  .B_INPUT("DIRECT"),               // Selects B input source, "DIRECT" (B port) or "CASCADE" (BCIN port)
+  .USE_DPORT("FALSE"),              // Select D port usage (TRUE or FALSE)
+  .USE_MULT("MULTIPLY"),            // Select multiplier usage ("MULTIPLY", "DYNAMIC", or "NONE")
+  .USE_SIMD("ONE48"),               // SIMD selection ("ONE48", "TWO24", "FOUR12")
+  // Pattern Detector Attributes: Pattern Detection Configuration
+  .AUTORESET_PATDET("NO_RESET"),    // "NO_RESET", "RESET_MATCH", "RESET_NOT_MATCH" 
+  .MASK(48'h3fffffffffff),          // 48-bit mask value for pattern detect (1=ignore)
+  .PATTERN(48'h000000000000),       // 48-bit pattern match for pattern detect
+  .SEL_MASK("MASK"),                // "C", "MASK", "ROUNDING_MODE1", "ROUNDING_MODE2" 
+  .SEL_PATTERN("PATTERN"),          // Select pattern value ("PATTERN" or "C")
+  .USE_PATTERN_DETECT("NO_PATDET"), // Enable pattern detect ("PATDET" or "NO_PATDET")
+  // Register Control Attributes: Pipeline Register Configuration
+  .ACASCREG(1),                     // Number of pipeline stages between A/ACIN and ACOUT (0, 1 or 2)
+  .ADREG(1),                        // Number of pipeline stages for pre-adder (0 or 1)
+  .ALUMODEREG(1),                   // Number of pipeline stages for ALUMODE (0 or 1)
+  .AREG(1),                         // Number of pipeline stages for A (0, 1 or 2)
+  .BCASCREG(1),                     // Number of pipeline stages between B/BCIN and BCOUT (0, 1 or 2)
+  .BREG(1),                         // Number of pipeline stages for B (0, 1 or 2)
+  .CARRYINREG(1),                   // Number of pipeline stages for CARRYIN (0 or 1)
+  .CARRYINSELREG(1),                // Number of pipeline stages for CARRYINSEL (0 or 1)
+  .CREG(1),                         // Number of pipeline stages for C (0 or 1)
+  .DREG(1),                         // Number of pipeline stages for D (0 or 1)
+  .INMODEREG(1),                    // Number of pipeline stages for INMODE (0 or 1)
+  .MREG(1),                         // Number of multiplier pipeline stages (0 or 1)
+  .OPMODEREG(1),                    // Number of pipeline stages for OPMODE (0 or 1)
+  .PREG(1)                          // Number of pipeline stages for P (0 or 1)
+)
+DSP48E1_inst (
+  // Cascade: 30-bit (each) output: Cascade Ports
+  .ACOUT(ACOUT),                   // 30-bit output: A port cascade output
+  .BCOUT(BCOUT),                   // 18-bit output: B port cascade output
+  .CARRYCASCOUT(CARRYCASCOUT),     // 1-bit output: Cascade carry output
+  .MULTSIGNOUT(MULTSIGNOUT),       // 1-bit output: Multiplier sign cascade output
+  .PCOUT(PCOUT),                   // 48-bit output: Cascade output
+  // Control: 1-bit (each) output: Control Inputs/Status Bits
+  .OVERFLOW(OVERFLOW),             // 1-bit output: Overflow in add/acc output
+  .PATTERNBDETECT(PATTERNBDETECT), // 1-bit output: Pattern bar detect output
+  .PATTERNDETECT(PATTERNDETECT),   // 1-bit output: Pattern detect output
+  .UNDERFLOW(UNDERFLOW),           // 1-bit output: Underflow in add/acc output
+  // Data: 4-bit (each) output: Data Ports
+  .CARRYOUT(CARRYOUT),             // 4-bit output: Carry output
+  .P(P),                           // 48-bit output: Primary data output
+  // Cascade: 30-bit (each) input: Cascade Ports
+  .ACIN(ACIN),                     // 30-bit input: A cascade data input
+  .BCIN(BCIN),                     // 18-bit input: B cascade input
+  .CARRYCASCIN(CARRYCASCIN),       // 1-bit input: Cascade carry input
+  .MULTSIGNIN(MULTSIGNIN),         // 1-bit input: Multiplier sign input
+  .PCIN(PCIN),                     // 48-bit input: P cascade input
+  // Control: 4-bit (each) input: Control Inputs/Status Bits
+  .ALUMODE(ALUMODE),               // 4-bit input: ALU control input
+  .CARRYINSEL(CARRYINSEL),         // 3-bit input: Carry select input
+  .CLK(CLK),                       // 1-bit input: Clock input
+  .INMODE(INMODE),                 // 5-bit input: INMODE control input
+  .OPMODE(OPMODE),                 // 7-bit input: Operation mode input
+  // Data: 30-bit (each) input: Data Ports
+  .A(operand1),                           // 30-bit input: A data input
+  .B(operand2),                           // 18-bit input: B data input
+  .C(operand3),                           // 48-bit input: C data input
+  .CARRYIN(flag[0]),               // 1-bit input: Carry input signal
+  .D(operand4),                           // 25-bit input: D data input
+  // Reset/Clock Enable: 1-bit (each) input: Reset/Clock Enable Inputs
+  .CEA1(CEA1),                     // 1-bit input: Clock enable input for 1st stage AREG
+  .CEA2(CEA2),                     // 1-bit input: Clock enable input for 2nd stage AREG
+  .CEAD(CEAD),                     // 1-bit input: Clock enable input for ADREG
+  .CEALUMODE(CEALUMODE),           // 1-bit input: Clock enable input for ALUMODE
+  .CEB1(CEB1),                     // 1-bit input: Clock enable input for 1st stage BREG
+  .CEB2(CEB2),                     // 1-bit input: Clock enable input for 2nd stage BREG
+  .CEC(CEC),                       // 1-bit input: Clock enable input for CREG
+  .CECARRYIN(CECARRYIN),           // 1-bit input: Clock enable input for CARRYINREG
+  .CECTRL(CECTRL),                 // 1-bit input: Clock enable input for OPMODEREG and CARRYINSELREG
+  .CED(CED),                       // 1-bit input: Clock enable input for DREG
+  .CEINMODE(CEINMODE),             // 1-bit input: Clock enable input for INMODEREG
+  .CEM(CEM),                       // 1-bit input: Clock enable input for MREG
+  .CEP(CEP),                       // 1-bit input: Clock enable input for PREG
+  .RSTA(RSTA),                     // 1-bit input: Reset input for AREG
+  .RSTALLCARRYIN(RSTALLCARRYIN),   // 1-bit input: Reset input for CARRYINREG
+  .RSTALUMODE(RSTALUMODE),         // 1-bit input: Reset input for ALUMODEREG
+  .RSTB(RSTB),                     // 1-bit input: Reset input for BREG
+  .RSTC(RSTC),                     // 1-bit input: Reset input for CREG
+  .RSTCTRL(RSTCTRL),               // 1-bit input: Reset input for OPMODEREG and CARRYINSELREG
+  .RSTD(RSTD),                     // 1-bit input: Reset input for DREG and ADREG
+  .RSTINMODE(RSTINMODE),           // 1-bit input: Reset input for INMODEREG
+  .RSTM(RSTM),                     // 1-bit input: Reset input for MREG
+  .RSTP(RSTP)                      // 1-bit input: Reset input for PREG
+);
 
 
 Tile_Checker Chk1(
@@ -139,58 +232,58 @@ always @(posedge clk) begin
     if(!program_mode && instructions[ip][63] != 'b0) begin
         case (instructions[ip] [2:0])
             3'b000 : 
-                registers[instructions[ip][14:11]] = ALUOUT;
+                registers[instructions[ip][22:19]] = ALUOUT;
             3'b001: begin
             // Send data to data Memory
                 data_mem_cntrl = 1'b1;
                 data_mem_valid = 1'b1;
-                data_mem_wrt_addr = instructions[ip][27:18];
+                data_mem_wrt_addr = instructions[ip][35:26];
                 data_mem_wrt_data = registers[instructions[ip][31:28]];
             end
             3'b010: begin
                 data_mem_cntrl = 1'b0;
-                data_mem_rd_addr = instructions[ip][27:18];
+                data_mem_rd_addr = instructions[ip][35:26];
                 registers[instructions[ip][31:28]] = data_from_mem;
                 data_mem_rd_valid = data_valid_from_mem;
             end
             3'b011: begin
-                case (instructions[ip][17:15])
+                case (instructions[ip][25:23])
                     3'b000: begin
-                        send_to_tile_data[31:0] = registers[instructions[ip][38:35]];
-                        send_to_tile_addr[2:0] = registers[instructions[ip][17:15]];
+                        send_to_tile_data[31:0] = registers[instructions[ip][44:41]];
+                        send_to_tile_addr[2:0] = registers[instructions[ip][25:23]];
                     end
                     3'b001: begin
-                        send_to_tile_data[63:32] = registers[instructions[ip][38:35]];
-                        send_to_tile_addr[6:3] = registers[instructions[ip][17:15]];
+                        send_to_tile_data[63:32] = registers[instructions[ip][44:41]];
+                        send_to_tile_addr[6:3] = registers[instructions[ip][25:23]];
                     end
                     3'b010: begin
-                        send_to_tile_data[95:64] = registers[instructions[ip][38:35]];
-                        send_to_tile_addr[8:6] = registers[instructions[ip][17:15]];
+                        send_to_tile_data[95:64] = registers[instructions[ip][44:41]];
+                        send_to_tile_addr[8:6] = registers[instructions[ip][25:23]];
                     end
                     3'b011: begin
-                        send_to_tile_data[127:96] = registers[instructions[ip][38:35]];
-                        send_to_tile_addr[11:9] = registers[instructions[ip][17:15]];
+                        send_to_tile_data[127:96] = registers[instructions[ip][44:41]];
+                        send_to_tile_addr[11:9] = registers[instructions[ip][25:23]];
                     end
                     3'b100: begin
-                        send_to_tile_data[159:128] = registers[instructions[ip][38:35]];
-                        send_to_tile_addr[14:12] = registers[instructions[ip][17:15]];
+                        send_to_tile_data[159:128] = registers[instructions[ip][44:41]];
+                        send_to_tile_addr[14:12] = registers[instructions[ip][25:23]];
                     end
                     3'b101: begin
-                        send_to_tile_data[191:160] = registers[instructions[ip][38:35]];
-                        send_to_tile_addr[17:15] = registers[instructions[ip][17:15]];
+                        send_to_tile_data[191:160] = registers[instructions[ip][44:41]];
+                        send_to_tile_addr[17:15] = registers[instructions[ip][25:23]];
                     end
                     3'b110: begin
-                        send_to_tile_data[223:192] = registers[instructions[ip][38:35]];
-                        send_to_tile_addr[20:18] = registers[instructions[ip][17:15]];
+                        send_to_tile_data[223:192] = registers[instructions[ip][44:41]];
+                        send_to_tile_addr[20:18] = registers[instructions[ip][25:23]];
                     end
                     3'b111: begin
-                        send_to_tile_data[255:224] = registers[instructions[ip][38:35]];
-                        send_to_tile_addr[23:21] = registers[instructions[ip][17:15]];
+                        send_to_tile_data[255:224] = registers[instructions[ip][44:41]];
+                        send_to_tile_addr[23:21] = registers[instructions[ip][25:23]];
                     end
                 endcase
             end
             3'b100: begin
-                case (instructions[ip][17:15])
+                case (instructions[ip][25:23])
                     3'b000: begin
                         registers[recv_from_tile_addr] = recv_from_tile_data[31:0];
                     end
